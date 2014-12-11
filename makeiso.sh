bb42104847758bb682182071652510ba3dc096ec
André Vorbach <andre@vorbach.org>
2014-12-11 10:13:38 +0100

    Updated package to syslinux

--- makeiso.sh
+++ makeiso.sh
@@ -25,7 +25,7 @@ fuECHO () {
 # Let's install all the packages we need
 fuECHO "### Installing packages."
 apt-get update -y
-apt-get install genisoimage isolinux -y
+apt-get install genisoimage syslinux -y
 
 # Let's get Ubuntu 14.04.1 as .iso
 fuECHO "### Downloading Ubuntu 14.04.1."
