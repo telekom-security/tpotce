333da3ad62a72c38d04feadf8a11911086ce8991
t3chn0m4g3 <marco.ochse@gmail.com>
2014-12-18 18:38:32 +0100

    fix wakeup from console
    some device / monitor combinations have a problem waking up from consoleblank

--- installer/install1.sh
+++ installer/install1.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #############################################################
-# T-Pot Community Edition - disable splash boot permanently #
-# Ubuntu server 14.04, x64                                  #
+# T-Pot Community Edition - disable splash boot             #
+#                           and consoleblank permanently    #
+# Ubuntu server 14.04.1, x64                                #
 #                                                           #
-# v0.04 by mo, 2014-11-28                                   #
+# v0.05 by mo, DTAG, 2014-12-18                             #
 #############################################################
 
-# Let's comment out the "quiet splash" options and update grub
-sed -i.bak 's#GRUB_CMDLINE_LINUX_DEFAULT#\#GRUB_CMDLINE_LINUX_DEFAULT#' /etc/default/grub
+# Let's replace "quiet splash" options and update grub
+sed -i.bak 's#GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"#GRUB_CMDLINE_LINUX_DEFAULT="consoleblank=0"#' /etc/default/grub
 update-grub
 
 # Let's move the install script to rc.local and reboot
