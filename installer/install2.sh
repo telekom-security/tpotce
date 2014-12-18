3d6aee54e3fc66ffc081a5e9fc1e724174077271
t3chn0m4g3 <marco.ochse@gmail.com>
2014-12-18 18:24:00 +0100

    clear /dev/tty2 before output

--- installer/install2.sh
+++ installer/install2.sh
@@ -232,7 +232,7 @@ fuECHO "### Adding useful docker output to tty2"
 tee -a /etc/crontab <<EOF
 
 # Show running containers every 60s via /dev/tty2
-*/1 * * * * root date > /dev/tty2; docker ps > /dev/tty2; echo > /dev/tty2
+*/1 * * * * root clear > /dev/tty2; date > /dev/tty2; docker ps > /dev/tty2; echo > /dev/tty2
 EOF
 
 # Let's add a nice and useful issue text and update rc.local accordingly
