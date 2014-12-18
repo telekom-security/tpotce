02a6777f1be779f21bdb4bcc352f109e98c43f77
t3chn0m4g3 <marco.ochse@gmail.com>
2014-12-18 18:15:13 +0100

    fix typo

--- installer/install2.sh
+++ installer/install2.sh
@@ -3,7 +3,7 @@
 # T-Pot Community Edition post install script          #
 # Ubuntu server 14.04, x64                             #
 #                                                      #
-# v0.17 by mo, DTAG, 2014-12-18                        #
+# v0.18 by mo, DTAG, 2014-12-18                        #
 ########################################################
 
 # Let's make sure there is a warning if running for a second time
@@ -232,7 +232,7 @@ fuECHO "### Adding useful docker output to tty2"
 tee -a /etc/crontab <<EOF
 
 # Show running containers every 60s via /dev/tty2
-*/1 * * * * root date > /dev/tty2; docker ps > /dev/tty2; echo /dev/tty2
+*/1 * * * * root date > /dev/tty2; docker ps > /dev/tty2; echo > /dev/tty2
 EOF
 
 # Let's add a nice and useful issue text and update rc.local accordingly
@@ -257,6 +257,7 @@ IP:
 
 CTRL+ALT+F2 - Display current container status
 CTRL+ALT+F1 - Return to this screen
+
 EOF
 
 echo "#!/bin/sh -e" > /etc/rc.local.new
