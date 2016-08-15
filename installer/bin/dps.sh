bd29236c47fd0e8ab50081f978a83640f509f32d
t3chn0m4g3 <t3chn0m4g3@gmail.com>
2016-08-15 05:46:57 +0000

    tweaking dps.sh

--- installer/bin/dps.sh
+++ installer/bin/dps.sh
@@ -1,2 +1,17 @@
 #/bin/bash
-docker ps --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}" -f status=running -f status=exited | GREP_COLORS='mt=01;35' egrep --color=always "(^[_a-z-]+ |$)|$" | GREP_COLORS='mt=01;32' egrep --color=always "(Up[ 0-9a-Z ]+ |$)|$" | GREP_COLORS='mt=01;31' egrep --color=always "(Exited[ \(0-9\) ]+ [0-9a-Z ]+ ago|$)|$"
+while true
+  do
+    clear
+    echo "======| System |======"
+    echo Date:"     "$(date)
+    echo Uptime:"  "$(uptime)
+    echo CPU temp: $(sensors | grep "Physical" | awk '{ print $4 }')
+    echo
+    /usr/bin/docker ps --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}" -f status=running -f status=exited | GREP_COLORS='mt=01;35' /bin/egrep --color=always "(^[_a-z-]+ |$)|$" | GREP_COLORS='mt=01;32' /bin/egrep --color=always "(Up[ 0-9a-Z ]+ |$)|$" | GREP_COLORS='mt=01;31' /bin/egrep --color=always "(Exited[ \(0-9\) ]+ [0-9a-Z ]+ ago|$)|$"
+  if [ "$1" = "" ];
+    then 
+      break;
+    else 
+      sleep $1
+  fi
+done
