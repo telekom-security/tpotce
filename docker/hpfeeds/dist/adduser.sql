fbc11fa12ccfc2b36c503348451c5f07ca81365c
Marco Ochse <t3chn0m4g3@gmail.com>
2018-05-30 15:46:18 +0000

    add hpfeeds broker for testing

---
+++ docker/hpfeeds/dist/adduser.sql
@@ -0,0 +1,1 @@
+insert into authkeys (owner, ident, secret, pubchans, subchans) values ('testID', 'testID', 'testSecret', '["ciscoasa", "rdpy-channel", "mailoney.mail","mailoney.commands",mailoney.shellcode"]', '["ciscoasa", "rdpy-channel", "mailoney.mail","mailoney.commands",mailoney.shellcode"]');
