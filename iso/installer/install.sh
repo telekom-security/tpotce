4efc62b529c558e88fee3f7ce50c9812df70463e
Sebastian Haderecker <sebastian.haderecker@gmail.com>
2019-05-16 08:14:05 +0000

    Revert "Adapt testing paths"
    This reverts commit 8b2898bd4e7a0f3c729b962998ccd79ac61ddc61.

--- iso/installer/install.sh
+++ iso/installer/install.sh
@@ -677,7 +677,7 @@ pip install elasticsearch-curator yq
 
 # Cloning T-Pot from GitHub
 fuBANNER "Cloning T-Pot"
-git clone https://github.com/TheHADILP/tpotce.git -b hpfeeds-cmd /opt/tpot
+git clone https://github.com/dtag-dev-sec/tpotce /opt/tpot
 
 # Let's create the T-Pot user
 fuBANNER "Create user"
