eb22f8e99b1afd66ae5470211054eebbc094cef6
t3chn0m4g3 <t3chn0m4g3@gmail.com>
2019-08-03 00:20:46 +0200

    final prep for buster

--- update.sh
+++ update.sh
@@ -77,7 +77,7 @@ echo
 # Let's check for version
 function fuCHECK_VERSION () {
 local myMINVERSION="19.03.0"
-local myMASTERVERSION="19.03.0"
+local myMASTERVERSION="19.03.1"
 echo
 echo "### Checking for Release ID"
 myRELEASE=$(lsb_release -i | grep Debian -c)
