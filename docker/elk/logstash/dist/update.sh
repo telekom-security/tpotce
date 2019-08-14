31d7707d1994bc9552e6f1ff22bd4f7dde7594de
t3chn0m4g3 <t3chn0m4g3@gmail.com>
2019-08-14 14:43:47 +0000

    download instead of git pull
    download translation maps rather than running a git pull
    translation maps will now be bzip2 compressed to reduce traffic to a minimum
    fixes #432

--- docker/elk/logstash/dist/update.sh
+++ docker/elk/logstash/dist/update.sh
@@ -6,7 +6,31 @@ function fuCLEANUP {
 }
 trap fuCLEANUP EXIT
 
-# Download updated translation maps
-cd /etc/listbot 
-git pull --all --depth=1
-cd /
+# Check internet availability 
+function fuCHECKINET () {
+mySITES=$1
+error=0
+for i in $mySITES;
+  do
+    curl --connect-timeout 5 -Is $i 2>&1 > /dev/null
+      if [ $? -ne 0 ];
+        then
+          let error+=1
+      fi;
+  done;
+  echo $error
+}
+
+# Check for connectivity and download latest translation maps
+myCHECK=$(fuCHECKINET "raw.githubusercontent.com")
+if [ "$myCHECK" == "0" ];
+  then
+    echo "Connection to Github looks good, now downloading latest translation maps."
+    cd /etc/listbot 
+    aria2c -s16 -x 16 https://raw.githubusercontent.com/dtag-dev-sec/listbot/master/cve.yaml.bz2 && \
+    aria2c -s16 -x 16 https://raw.githubusercontent.com/dtag-dev-sec/listbot/master/iprep.yaml.bz2 && \
+    bunzip2 -f *.bz2
+    cd /
+  else
+    echo "Cannot reach Github, starting Logstash without latest translation maps."
+fi
