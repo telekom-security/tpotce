5badf352be6b92566743befe5c88aef9f2589cf0
Marco Ochse <t3chn0m4g3@users.noreply.github.com>
2020-03-15 21:11:26 +0100

    deal with changes in sid
    move to testing
    cockpit-docker removed upstream, remove here

--- iso/installer/install.sh
+++ iso/installer/install.sh
@@ -14,17 +14,17 @@ myLSB_STABLE_SUPPORTED="stretch buster"
 myLSB_TESTING_SUPPORTED="sid"
 myREMOTESITES="https://hub.docker.com https://github.com https://pypi.python.org https://debian.org"
 myPREINSTALLPACKAGES="aria2 apache2-utils cracklib-runtime curl dialog figlet fuse grc libcrack2 libpq-dev lsb-release netselect-apt net-tools software-properties-common toilet"
-myINSTALLPACKAGES="aria2 apache2-utils apparmor apt-transport-https aufs-tools bash-completion build-essential ca-certificates cgroupfs-mount cockpit cockpit-docker console-setup console-setup-linux cracklib-runtime curl debconf-utils dialog dnsutils docker.io docker-compose elasticsearch-curator ethtool fail2ban figlet genisoimage git glances grc haveged html2text htop iptables iw jq kbd libcrack2 libltdl7 libpam-google-authenticator man mosh multitail netselect-apt net-tools npm ntp openssh-server openssl pass pigz prips software-properties-common syslinux psmisc pv python3-pip toilet unattended-upgrades unzip vim wget wireless-tools wpasupplicant"
+myINSTALLPACKAGES="aria2 apache2-utils apparmor apt-transport-https aufs-tools bash-completion build-essential ca-certificates cgroupfs-mount cockpit console-setup console-setup-linux cracklib-runtime curl debconf-utils dialog dnsutils docker.io docker-compose elasticsearch-curator ethtool fail2ban figlet genisoimage git glances grc haveged html2text htop iptables iw jq kbd libcrack2 libltdl7 libpam-google-authenticator man mosh multitail netselect-apt net-tools npm ntp openssh-server openssl pass pigz prips software-properties-common syslinux psmisc pv python3-pip toilet unattended-upgrades unzip vim wget wireless-tools wpasupplicant"
 myINFO="\
 ########################################
-### T-Pot Installer for Debian (Sid) ###
+### T-Pot Installer for Debian (testing) ###
 ########################################
 
 Disclaimer:
 This script will install T-Pot on this system.
 By running the script you know what you are doing:
 1. SSH will be reconfigured to tcp/64295.
-2. Your Debian installation will be upgraded to Sid / unstable.
+2. Your Debian installation will be upgraded to testing.
 3. Please ensure other means of access to this system in case something goes wrong.
 4. At best this script will be executed on the console instead through a SSH session.
 
@@ -283,15 +283,15 @@ function fuGET_DEPS {
   echo
   echo "### Determine fastest mirror for your location."
   echo
-  netselect-apt -n -a amd64 unstable && cp sources.list /etc/apt/
-  mySOURCESCHECK=$(cat /etc/apt/sources.list | grep -c unstable)
+  netselect-apt -n -a amd64 testing && cp sources.list /etc/apt/
+  mySOURCESCHECK=$(cat /etc/apt/sources.list | grep -c testing)
   if [ "$mySOURCESCHECK" == "0" ]
     then
       echo "### Automatic mirror selection failed, using main mirror."
-      # Point to Debian (Sid, unstable)
+      # Point to Debian (testing)
 tee /etc/apt/sources.list <<EOF
-deb http://deb.debian.org/debian unstable main contrib non-free
-deb-src http://deb.debian.org/debian unstable main contrib non-free
+deb http://deb.debian.org/debian testing main contrib non-free
+deb-src http://deb.debian.org/debian testing main contrib non-free
 EOF
   fi
   echo
