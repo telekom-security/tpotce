840662da48e81a5940240e16c3fec7d48a3846f5
Sebastian Haderecker <sebastian.haderecker@gmail.com>
2020-08-26 11:21:55 +0200

    Update OTC Debian 10 base image id

--- cloud/terraform/otc/variables.tf
+++ cloud/terraform/otc/variables.tf
@@ -50,7 +50,7 @@ variable "key_pair" {
 }
 
 variable "image_id" {
-  default = "d97dd29c-9318-4e4c-8d3a-7307d1513b77"
+  default = "fb7b0c9c-8b20-4e3f-832c-ea38c981c282"
   description = "Select a Debian 10 base image id"
 }
 
