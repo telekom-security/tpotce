5fd0d158e67036c7527f23079d6ff162e563f521
t3chn0m4g3 <t3chn0m4g3@gmail.com>
2023-04-27 18:42:38 +0200

    Add Nginx Cockpit Awareness

---
+++ docker/nginx/dist/html/config_light.js
@@ -0,0 +1,71 @@
+// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
+// ╠╩╗║╣ ║║║ ║ ║ ║
+// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
+// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
+// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
+// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+
+const CONFIG = {
+  // ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+  // ├┴┐├─┤└─┐││  └─┐
+  // └─┘┴ ┴└─┘┴└─┘└─┘
+
+  // General
+  imageBackground: true,
+  openInNewTab: true,
+  twelveHourFormat: false,
+
+  // Greetings
+  greetingMorning: 'Good morning ☕',
+  greetingAfternoon: 'Good afternoon 🍯',
+  greetingEvening: 'Good evening 😁',
+  greetingNight: 'Go to Sleep 🥱',
+
+  // ┬  ┬┌─┐┌┬┐┌─┐
+  // │  │└─┐ │ └─┐
+  // ┴─┘┴└─┘ ┴ └─┘
+
+  //Icons
+  firstListIcon: 'home',
+  secondListIcon: 'external-link',
+
+  // Links
+  lists: {
+    firstList: [
+      {
+        name: 'Attack Map',
+        link: '/map/',
+      },
+      {
+        name: 'Cyberchef',
+        link: '/cyberchef/',
+      },
+      {
+        name: 'Elasticvue',
+        link: '/elasticvue/',
+      },
+      {
+        name: 'Kibana',
+        link: '/kibana/',
+      },
+      {
+        name: 'Spiderfoot',
+        link: '/spiderfoot/',
+      },
+    ],
+    secondList: [
+      {
+        name: 'SecurityMeter',
+        link: 'https://sicherheitstacho.eu',
+      },
+      {
+        name: 'T-Pot @ GitHub',
+        link: 'https://github.com/telekom-security/tpotce/',
+      },
+      {
+        name: 'T-Pot ReadMe',
+        link: 'https://github.com/telekom-security/tpotce/blob/master/README.md',
+      },      
+    ],
+  },
+};
