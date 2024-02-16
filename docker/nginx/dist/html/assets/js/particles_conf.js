0dda858ac1b6cea4f54f4aea913b8dd7ad234c66
Marco Ochse <t3chn0m4g3@users.noreply.github.com>
2024-02-16 19:15:17 +0100

    Start working on new landing page Remove old code

---
+++ docker/nginx/dist/html/assets/js/particles_conf.js
@@ -0,0 +1,54 @@
+particlesJS('particles-js', {
+  particles: {
+    number: { value: 80, density: { enable: true, value_area: 800 } },
+    color: { value: '#e20074' },
+    shape: {
+      type: 'polygon',
+      stroke: { width: 0, color: '#000000' },
+      polygon: { nb_sides: 6 },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: { enable: true, speed: 1, opacity_min: 0.1, sync: false }
+    },
+    size: {
+      value: 6,
+      random: true,
+      anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: '#e20074',
+      opacity: 0.4,
+      width: 2
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: 'none',
+      random: false,
+      straight: false,
+      out_mode: 'out',
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 1200 }
+    },
+  },
+  interactivity: {
+    detect_on: 'canvas',
+    events: {
+      onhover: { enable: true, mode: 'repulse' },
+      onclick: { enable: true, mode: 'push' },
+      resize: true
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+      repulse: { distance: 200, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 }
+    }
+  },
+  retina_detect: true
+});
