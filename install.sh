127f0c2c924295fb7230ed0b1b38ccfc76f8ce69
t3chn0m4g3 <t3chn0m4g3@gmail.com>
2024-02-23 17:30:36 +0100

    point installer and ansible to alpha

--- install.sh
+++ install.sh
@@ -118,7 +118,7 @@ fi
 if [ ! -f installer/install/tpot.yml ] && [ ! -f tpot.yml ];
   then
     echo "### Now downloading T-Pot Ansible Installation Playbook ... "
-    wget -qO tpot.yml https://github.com/telekom-security/tpotce/raw/dev/installer/install/tpot.yml
+    wget -qO tpot.yml https://github.com/telekom-security/tpotce/raw/alpha/installer/install/tpot.yml
     myANSIBLE_TPOT_PLAYBOOK="tpot.yml"
     echo
   else
