4baac7ac04494a27bdd25c9cedd6fcf932be6858
t3chn0m4g3 <t3chn0m4g3@gmail.com>
2024-03-11 09:45:01 +0100

    update esvue, cleanup

--- docker/nginx/builder/esvue/build.sh
+++ docker/nginx/builder/esvue/build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Needs buildx to build. Run tpotce/bin/setup-builder.sh first
-echo "do not build!"
-exit 0
 docker buildx build --no-cache --progress plain --output ../../dist/html/esvue/ .
