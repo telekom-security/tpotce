a8ea154ed516a668cb6b7c7c77b29e1e2c387b09
t3chn0m4g3 <t3chn0m4g3@gmail.com>
2026-05-28 14:55:45 +0200

    have tests with identical tags

--- docker/_tests/tests/cowrie.sh
+++ docker/_tests/tests/cowrie.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 source "${SCRIPT_DIR}/../lib/common.sh"
 
 TEST_NAME="cowrie"
-DEFAULT_IMAGE="ghcr.io/telekom-security/cowrie:24.04.1"
+DEFAULT_IMAGE="dtagdevsec/cowrie:24.04.1"
 IMAGE=""
 SSH_PORT=""
 TELNET_PORT=""
